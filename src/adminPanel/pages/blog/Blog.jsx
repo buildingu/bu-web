5b44bb9f2b8b32eaf0d557b7d521cb9f34b968ba
David <davidbish2002@hotmail.com>
2024-08-08 21:59:16 -0230

    adminPanel blog placeholder code.

--- src/adminPanel/pages/blog/Blog.jsx
+++ src/adminPanel/pages/blog/Blog.jsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
+import { Table } from "../../components/table";
+
+// import api from "./services"
+
 import s from "./blog.module.css";
 
 const Blog = () => {
-  return <main className={s.container}>Blog</main>;
+  const [loading, setLoading] = useState(false),
+    [blogData, setBlogData] = useState({});
+
+  const getBlogs = async () => {
+    // const res = await fetch("url");
+    // const data = await res.json();
+    // setBlogData(data)
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    // On client load.
+    getBlogs().finally(() => setLoading(false)); // or you can just have this finally in getBlogs...
+  }, []);
+
+  //   useEffect(() => {
+  //     // On client load too.
+  //     if (blogData) {
+  //       // Runs when blogData changes.
+  //       // something
+  //     }
+  //   }, [blogData]);
+
+  return (
+    <main className={s.container}>
+      {loading ? <p>loading...</p> : <Table data={blogData} />}
+    </main>
+  );
 };
 
 export default Blog;
