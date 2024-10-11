24e7c7bd45f92837af4dd5cee78c8cfc47954880
Andy Xu <xuyuchen100@gmail.com>
2024-10-11 16:55:26 -0400

    updated structure

--- src/pages/s4yt/S4YT.jsx
+++ src/pages/s4yt/S4YT.jsx
@@ -63,57 +63,25 @@ const S4YT = () => {
       <main className={s.s4ytsection}>
         {/* <div> */}
         {/* TODO: Not section */}
-        <section className={s.s4ytLeftsec}>
-          <img
-            src="mockups/assets/dollarpage/logo_dollars.png"
-            alt="S4YT Logo"
-            className={s.s4ytPaper}
-          />  
-          <img
-            src="mockups/assets/dollarpage/click-me.png"
-            alt="Click Me"
-            className={s.s4ytClick}
-          />
-        </section>
-        {!visibleSection ? null : <button className = {s.backButton} onClick={handleBackClick}> Back </button>}
-        
-        {/* <div className="contentContainer"> */}
-        <div className={s.contentContainer}>
-          {/* TODO: Get rid and just change the buttons with media query(ies). */}
-          <div className={s.s4ytInfobuttonsDesktop}>
+        <div className={s.leftRightContainer}>
+          <div className={s.s4ytLeftsec}>
             <img
-              src={getImageSource(0)}
-              onClick={() => handleSectionClick(1)}
-              alt="What is S4YT"
-              className={visibleSection === 1 ? s.pressed : s.s4ytWhat}
-            />
+              src="mockups/assets/dollarpage/logo_dollars.png"
+              alt="S4YT Logo"
+              className={s.s4ytPaper}
+            />  
             <img
-              src={getImageSource(1)}
-              onClick={() => handleSectionClick(2)}
-              alt="Am I Eligible"
-              className={visibleSection === 2 ? s.pressed : s.s4ytAmi}
-            />
-            <img
-              src={getImageSource(2)}
-              onClick={() => handleSectionClick(3)}
-              alt="When can I submit"
-              className={visibleSection === 3 ? s.pressed : s.s4ytWhen}
-            />
-            <img
-              src={getImageSource(3)}
-              onClick={() => handleSectionClick(4)}
-              alt="How do I submit"
-              className={visibleSection === 4 ? s.pressed : s.s4ytHowdo}
-            />
-            <img
-              src={getImageSource(4)}
-              onClick={() => handleSectionClick(5)}
-              alt="What are the prizes"
-              className={visibleSection === 5 ? s.pressed : s.s4ytWhatare}
+              src="mockups/assets/dollarpage/click-me.png"
+              alt="Click Me"
+              className={s.s4ytClick}
             />
           </div>
-          {(visibleSection == null) ? (
-            <div className={s.s4ytInfobuttonsMobile}>
+          {!visibleSection ? null : <button className = {s.backButton} onClick={handleBackClick}> Back </button>}
+          
+          {/* <div className="contentContainer"> */}
+          <div className={s.contentContainer}>
+            {/* TODO: Get rid and just change the buttons with media query(ies). */}
+            <div className={s.s4ytInfobuttonsDesktop}>
               <img
                 src={getImageSource(0)}
                 onClick={() => handleSectionClick(1)}
@@ -145,89 +113,122 @@ const S4YT = () => {
                 className={visibleSection === 5 ? s.pressed : s.s4ytWhatare}
               />
             </div>
-          ) : null}
+            {(visibleSection == null) ? (
+              <div className={s.s4ytInfobuttonsMobile}>
+                <img
+                  src={getImageSource(0)}
+                  onClick={() => handleSectionClick(1)}
+                  alt="What is S4YT"
+                  className={visibleSection === 1 ? s.pressed : s.s4ytWhat}
+                />
+                <img
+                  src={getImageSource(1)}
+                  onClick={() => handleSectionClick(2)}
+                  alt="Am I Eligible"
+                  className={visibleSection === 2 ? s.pressed : s.s4ytAmi}
+                />
+                <img
+                  src={getImageSource(2)}
+                  onClick={() => handleSectionClick(3)}
+                  alt="When can I submit"
+                  className={visibleSection === 3 ? s.pressed : s.s4ytWhen}
+                />
+                <img
+                  src={getImageSource(3)}
+                  onClick={() => handleSectionClick(4)}
+                  alt="How do I submit"
+                  className={visibleSection === 4 ? s.pressed : s.s4ytHowdo}
+                />
+                <img
+                  src={getImageSource(4)}
+                  onClick={() => handleSectionClick(5)}
+                  alt="What are the prizes"
+                  className={visibleSection === 5 ? s.pressed : s.s4ytWhatare}
+                />
+              </div>
+            ) : null}
 
-          {/* TODO: Inline styles are confusing when trying to figure out css if you come back or there is a new dev or something. */}
-          {visibleSection === 1 && (
-            <div className={`${s.textInfo} ${s.txt1}`}>
-              <hgroup>
-                <h3 className={s.textHeader}>WHAT IS S4YT ABOUT?</h3>
-                {/* TODO: This is not showing? */}
-                <div className={s.line} />
-              </hgroup>
-              <ul>
-                <li className={s.listinfo}>Explore fun questions posed by businesses to win $$$</li>
-                <li className={s.listinfo}>Earn Dubl-U-nes to win prizes</li>
-                <li className={s.listinfo}>Sign up for post event meet-ups to learn more</li>
-                <li className={s.listinfo}>Join our <Link to="#"><img src={"/mockups/assets/dollarpage/logo-discord.png"} width={20} height={20} alt="Discord" /></Link> to learn more and stay updated!</li>
-              </ul>
-              <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
-            </div>
-          )}
-          {visibleSection === 2 && (
-            <div className={`${s.textInfo} ${s.txt2}`}>
-              <hgroup>
-                <h3 className={s.textHeader}>AM I ELIGIBLE?</h3>
-                {/* TODO: This is not showing? */}
-                <div className={s.line} />
-              </hgroup>
-              <ul>
-                <li className={s.listinfo}>Yes, if you are in grades 9-12 at the time of the contest!</li>
-                <li className={s.listinfo}>
-                  Students register for FREE and are assigned an ID# to play
-                </li>
-                <li className={s.listinfo}>Join our <Link to="#"><img src={"/mockups/assets/dollarpage/logo-discord.png"} width={20} height={20} alt="Discord" /></Link> to learn more and stay updated!</li>
-              </ul>
-              <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
-            </div>
-          )}
-          {visibleSection === 3 && (
-            <div className={`${s.textInfo} ${s.txt3}`}>
-              <hgroup>
-                <h3 className={s.textHeader}>WHEN CAN I ATTEND?</h3>
-                {/* TODO: This is not showing? */}
-                <div className={s.line} />
-              </hgroup>
-              <ul>
-                <li className={s.listinfo}>The event takes place in February 2025 but registration starts in September 2024!</li>
-              </ul>
-              <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
-            </div>
-          )}
-          {visibleSection === 4 && (
-            <div className={`${s.textInfo} ${s.txt4}`}>
-              <hgroup>
-                <h3 className={s.textHeader}>HOW DO I WIN $$?</h3>
-                {/* TODO: This is not showing? */}
-                <div className={s.line} />
-              </hgroup>
-              <ul>
-                <li className={s.listinfo}>{`Students may submit 1 (and only 1) answer to each business question, but may win multiple $$$ awards`}</li>
-                <li className={s.listinfo}>All student identity info and submission remain anonymous until prizes are awarded</li>
-                <li className={s.listinfo}>{`Students have 4 years to use $$(FUN-ding) on anything specific that will help foster success in the future`}</li>
-                <li className={s.listinfo}>Join our <Link to="#"><img src={"/mockups/assets/dollarpage/logo-discord.png"} width={20} height={20} alt="Discord" /></Link> to learn more and stay updated!</li>
-              </ul>
-              <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
-            </div>
-          )}
-          {visibleSection === 5 && (
-            <div className={`${s.textInfo} ${s.txt5}`}>
-              <hgroup>
-                <h3 className={s.textHeader}>WHAT ARE DUBL-U-NES?</h3>
-                {/* TODO: This is not showing? */}
-                <div className={s.line} />
-              </hgroup>
-              <ul>
-                <li className={s.listinfo}>Dubl-U-nes can be used to win prizes donated by our Raffle Partners</li>
-                <li className={s.listinfo}>All registered students receive 3 free Dubl-U-nes</li>
-                <li className={s.listinfo}>Students assign their Dubl-U-nes to selected raffle items over the course of the event</li>
-                <li className={s.listinfo}>{`Students can earn more Dubl-U-nes (once they register for the game) through a variety of social media interactions`}</li>
-                <li className={s.listinfo}>Join our <Link to="#"><img src={"/mockups/assets/dollarpage/logo-discord.png"} width={20} height={20} alt="Discord" /></Link> to learn more about earning Dubl-U-nes!</li>
-              </ul>
-              <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
-            </div>
-          )}
-        
+            {visibleSection === 1 && (
+              <div className={`${s.textInfo} ${s.txt1}`}>
+                <hgroup>
+                  <h3 className={s.textHeader}>WHAT IS S4YT ABOUT?</h3>
+                  {/* TODO: This is not showing? */}
+                  <div className={s.line} />
+                </hgroup>
+                <ul>
+                  <li className={s.listinfo}>Explore fun questions posed by businesses to win $$$</li>
+                  <li className={s.listinfo}>Earn Dubl-U-nes to win prizes</li>
+                  <li className={s.listinfo}>Sign up for post event meet-ups to learn more</li>
+                  <li className={s.listinfo}>Join our <Link to="#"><img src={"/mockups/assets/dollarpage/logo-discord.png"} width={20} height={20} alt="Discord" /></Link> to learn more and stay updated!</li>
+                </ul>
+                <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
+              </div>
+            )}
+            {visibleSection === 2 && (
+              <div className={`${s.textInfo} ${s.txt2}`}>
+                <hgroup>
+                  <h3 className={s.textHeader}>AM I ELIGIBLE?</h3>
+                  {/* TODO: This is not showing? */}
+                  <div className={s.line} />
+                </hgroup>
+                <ul>
+                  <li className={s.listinfo}>Yes, if you are in grades 9-12 at the time of the contest!</li>
+                  <li className={s.listinfo}>
+                    Students register for FREE and are assigned an ID# to play
+                  </li>
+                  <li className={s.listinfo}>Join our <Link to="#"><img src={"/mockups/assets/dollarpage/logo-discord.png"} width={20} height={20} alt="Discord" /></Link> to learn more and stay updated!</li>
+                </ul>
+                <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
+              </div>
+            )}
+            {visibleSection === 3 && (
+              <div className={`${s.textInfo} ${s.txt3}`}>
+                <hgroup>
+                  <h3 className={s.textHeader}>WHEN CAN I ATTEND?</h3>
+                  {/* TODO: This is not showing? */}
+                  <div className={s.line} />
+                </hgroup>
+                <ul>
+                  <li className={s.listinfo}>The event takes place in February 2025 but registration starts in September 2024!</li>
+                </ul>
+                <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
+              </div>
+            )}
+            {visibleSection === 4 && (
+              <div className={`${s.textInfo} ${s.txt4}`}>
+                <hgroup>
+                  <h3 className={s.textHeader}>HOW DO I WIN $$?</h3>
+                  {/* TODO: This is not showing? */}
+                  <div className={s.line} />
+                </hgroup>
+                <ul>
+                  <li className={s.listinfo}>{`Students may submit 1 (and only 1) answer to each business question, but may win multiple $$$ awards`}</li>
+                  <li className={s.listinfo}>All student identity info and submission remain anonymous until prizes are awarded</li>
+                  <li className={s.listinfo}>{`Students have 4 years to use $$(FUN-ding) on anything specific that will help foster success in the future`}</li>
+                  <li className={s.listinfo}>Join our <Link to="#"><img src={"/mockups/assets/dollarpage/logo-discord.png"} width={20} height={20} alt="Discord" /></Link> to learn more and stay updated!</li>
+                </ul>
+                <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
+              </div>
+            )}
+            {visibleSection === 5 && (
+              <div className={`${s.textInfo} ${s.txt5}`}>
+                <hgroup>
+                  <h3 className={s.textHeader}>WHAT ARE DUBL-U-NES?</h3>
+                  {/* TODO: This is not showing? */}
+                  <div className={s.line} />
+                </hgroup>
+                <ul>
+                  <li className={s.listinfo}>Dubl-U-nes can be used to win prizes donated by our Raffle Partners</li>
+                  <li className={s.listinfo}>All registered students receive 3 free Dubl-U-nes</li>
+                  <li className={s.listinfo}>Students assign their Dubl-U-nes to selected raffle items over the course of the event</li>
+                  <li className={s.listinfo}>{`Students can earn more Dubl-U-nes (once they register for the game) through a variety of social media interactions`}</li>
+                  <li className={s.listinfo}>Join our <Link to="#"><img src={"/mockups/assets/dollarpage/logo-discord.png"} width={20} height={20} alt="Discord" /></Link> to learn more about earning Dubl-U-nes!</li>
+                </ul>
+                <img className={s.infoImg} src={"mockups/assets/dollarpage/background_info.png"} alt="Background Info" />
+              </div>
+            )}
+          
+          </div>
         </div>
         {/* </div> */}
         {/* </div> */}
